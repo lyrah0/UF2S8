9d3b876c81a4519f7a59dcb35a8d7874ca65a7fb
lyrah0 <lyrah0a@gmail.com>
2026-05-01 07:19:59 +0300

    better utilize load/store offsets

--- software/graphics_demo.s
+++ software/graphics_demo.s
@@ -13,22 +13,24 @@ init_timer:
         LI      r0, <int_handler
         SB      r0, [a3+0x20]
         
-        LI      r7, >HW_GP_BASE
-        LI      r6, <HW_GP_BASE
+        LI      r7, >HW_BASE2
+        LI      r6, <HW_BASE2
         LI      r0, 0x01
-        SB      r0, [a3+OFS_HW_CTRL]
+        SB      r0, [a3+OFS2_HW_CTRL]
         LI      r0, 10
-        SB      r0, [a3+OFS_TIMER_HZ]
+        SB      r0, [a3+OFS2_TIMER_HZ]
 
         LI      r0, 0x80
         MOV     flags, r0
 init_graphics:
+        LI      r7, >HW_BASE2
+        LI      r6, <HW_BASE2
         LI      r0, 40                  ; 320
-        SB      r0, [a3+OFS_GFX_WIDTH]
+        SB      r0, [a3+OFS2_GFX_WIDTH]
         LI      r0, 25                  ; 200
-        SB      r0, [a3+OFS_GFX_HEIGHT]
-        ;LI      r0, 0x00                ; mode 0
-        ;SB      r0, [a3+OFS_GFX_CTRL]
+        SB      r0, [a3+OFS2_GFX_HEIGHT]
+        LI      r0, 0x00                ; mode 0
+        SB      r0, [a3+OFS2_GFX_CTRL]
 
 demos_start:
         BL      AL, clear_screen
@@ -44,52 +46,52 @@ idle_loop:
         B       AL, idle_loop
 
 clear_screen:
-        LI      r7, >HW_BLIT_BASE
-        LI      r6, <HW_BLIT_BASE
+        LI      r7, >HW_BASE2
+        LI      r6, <HW_BASE2
         LI      r0, 0x00
-        SB      r0, [a3+OFS_BLIT_DST_X_L]
-        SB      r0, [a3+OFS_BLIT_DST_X_H]
-        SB      r0, [a3+OFS_BLIT_DST_Y_L]
-        SB      r0, [a3+OFS_BLIT_DST_Y_H]
-        SB      r0, [a3+OFS_BLIT_COLOR]
+        SB      r0, [a3+OFS2_BLIT_DST_X_L]
+        SB      r0, [a3+OFS2_BLIT_DST_X_H]
+        SB      r0, [a3+OFS2_BLIT_DST_Y_L]
+        SB      r0, [a3+OFS2_BLIT_DST_Y_H]
+        SB      r0, [a3+OFS2_BLIT_COLOR]
         LI      r0, 0x40
-        SB      r0, [a3+OFS_BLIT_WIDTH_L]
+        SB      r0, [a3+OFS2_BLIT_WIDTH_L]
         LI      r0, 0x01
-        SB      r0, [a3+OFS_BLIT_WIDTH_H]
+        SB      r0, [a3+OFS2_BLIT_WIDTH_H]
         LI      r0, 0xC8
-        SB      r0, [a3+OFS_BLIT_HEIGHT_L]
+        SB      r0, [a3+OFS2_BLIT_HEIGHT_L]
         LI      r0, 0x00
-        SB      r0, [a3+OFS_BLIT_HEIGHT_H]
+        SB      r0, [a3+OFS2_BLIT_HEIGHT_H]
         LI      r0, CMD_FILL_RECT
-        SB      r0, [a3+OFS_BLIT_CMD]
+        SB      r0, [a3+OFS2_BLIT_CMD]
         RET
 
 
 draw_rects:
-        LI      r7, >HW_BLIT_BASE
-        LI      r6, <HW_BLIT_BASE
+        LI      r7, >HW_BASE2
+        LI      r6, <HW_BASE2
         LI      r0, 0x00
-        SB      r0, [a3+OFS_BLIT_DST_X_H]
-        SB      r0, [a3+OFS_BLIT_DST_Y_H]
-        SB      r0, [a3+OFS_BLIT_WIDTH_H]
-        SB      r0, [a3+OFS_BLIT_HEIGHT_H]
+        SB      r0, [a3+OFS2_BLIT_DST_X_H]
+        SB      r0, [a3+OFS2_BLIT_DST_Y_H]
+        SB      r0, [a3+OFS2_BLIT_WIDTH_H]
+        SB      r0, [a3+OFS2_BLIT_HEIGHT_H]
         LI      r0, 40          ; width
-        SB      r0, [a3+OFS_BLIT_WIDTH_L]
+        SB      r0, [a3+OFS2_BLIT_WIDTH_L]
         LI      r0, 64          ; height
-        SB      r0, [a3+OFS_BLIT_HEIGHT_L]
+        SB      r0, [a3+OFS2_BLIT_HEIGHT_L]
         LI      r4, 0x00        ; x
         LI      r5, 0x00        ; y
         LI      r1, 40          ; number of rects to draw
 draw_rects_loop:
-        SB      r0, [a3+OFS_BLIT_COLOR]
+        SB      r0, [a3+OFS2_BLIT_COLOR]
         ADD     r0, r0, 17
-        SB      r4, [a3+OFS_BLIT_DST_X_L]
-        SB      r5, [a3+OFS_BLIT_DST_Y_L]
+        SB      r4, [a3+OFS2_BLIT_DST_X_L]
+        SB      r5, [a3+OFS2_BLIT_DST_Y_L]
         ADD     r4, r4, 5
         ADD     r5, r5, 3
         PUSH    r0
         LI      r0, CMD_FILL_RECT
-        SB      r0, [a3+OFS_BLIT_CMD]
+        SB      r0, [a3+OFS2_BLIT_CMD]
         POP     r0
         CMA     r1, r1
         B       ZS, draw_rects_end
@@ -104,59 +106,59 @@ draw_rects_end:
 
 
 draw_sprites:
-        LI      r7, >HW_BLIT_BASE
-        LI      r6, <HW_BLIT_BASE
+        LI      r7, >HW_BASE2
+        LI      r6, <HW_BASE2
         LI      r0, 0x00
-        SB      r0, [a3+OFS_BLIT_DST_X_H]
-        SB      r0, [a3+OFS_BLIT_DST_Y_H]
-        SB      r0, [a3+OFS_BLIT_WIDTH_H]
-        SB      r0, [a3+OFS_BLIT_HEIGHT_H]
+        SB      r0, [a3+OFS2_BLIT_DST_X_H]
+        SB      r0, [a3+OFS2_BLIT_DST_Y_H]
+        SB      r0, [a3+OFS2_BLIT_WIDTH_H]
+        SB      r0, [a3+OFS2_BLIT_HEIGHT_H]
         LI      r0, 8          ; width
-        SB      r0, [a3+OFS_BLIT_WIDTH_L]
+        SB      r0, [a3+OFS2_BLIT_WIDTH_L]
         LI      r0, 8          ; height
-        SB      r0, [a3+OFS_BLIT_HEIGHT_L]
+        SB      r0, [a3+OFS2_BLIT_HEIGHT_L]
         LI      r4, 0x20        ; x
-        SB      r4, [a3+OFS_BLIT_DST_X_L]
+        SB      r4, [a3+OFS2_BLIT_DST_X_L]
         LI      r5, 0x10        ; y
-        SB      r5, [a3+OFS_BLIT_DST_Y_L]
+        SB      r5, [a3+OFS2_BLIT_DST_Y_L]
         LI      r0, 0x00        ; color
-        SB      r0, [a3+OFS_BLIT_COLOR]
+        SB      r0, [a3+OFS2_BLIT_COLOR]
         LI      r0, >sprite0
-        SB      r0, [a3+OFS_BLIT_SRC_X_H]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_H]
         LI      r0, <sprite0
-        SB      r0, [a3+OFS_BLIT_SRC_X_L]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_L]
         LI      r0, CMD_BLIT_MEM_TRANS
-        SB      r0, [a3+OFS_BLIT_CMD]
+        SB      r0, [a3+OFS2_BLIT_CMD]
         LI      r4, 0x30        ; x
-        SB      r4, [a3+OFS_BLIT_DST_X_L]
+        SB      r4, [a3+OFS2_BLIT_DST_X_L]
         LI      r5, 0x18        ; y
-        SB      r5, [a3+OFS_BLIT_DST_Y_L]
+        SB      r5, [a3+OFS2_BLIT_DST_Y_L]
         LI      r0, >sprite1
-        SB      r0, [a3+OFS_BLIT_SRC_X_H]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_H]
         LI      r0, <sprite1
-        SB      r0, [a3+OFS_BLIT_SRC_X_L]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_L]
         LI      r0, CMD_BLIT_MEM_TRANS
-        SB      r0, [a3+OFS_BLIT_CMD]
+        SB      r0, [a3+OFS2_BLIT_CMD]
         LI      r4, 0x40        ; x
-        SB      r4, [a3+OFS_BLIT_DST_X_L]
+        SB      r4, [a3+OFS2_BLIT_DST_X_L]
         LI      r5, 0x20        ; y
-        SB      r5, [a3+OFS_BLIT_DST_Y_L]
+        SB      r5, [a3+OFS2_BLIT_DST_Y_L]
         LI      r0, >sprite2
-        SB      r0, [a3+OFS_BLIT_SRC_X_H]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_H]
         LI      r0, <sprite2
-        SB      r0, [a3+OFS_BLIT_SRC_X_L]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_L]
         LI      r0, CMD_BLIT_MEM_TRANS
-        SB      r0, [a3+OFS_BLIT_CMD]
+        SB      r0, [a3+OFS2_BLIT_CMD]
         LI      r4, 0x50        ; x
-        SB      r4, [a3+OFS_BLIT_DST_X_L]
+        SB      r4, [a3+OFS2_BLIT_DST_X_L]
         LI      r5, 0x28        ; y
-        SB      r5, [a3+OFS_BLIT_DST_Y_L]
+        SB      r5, [a3+OFS2_BLIT_DST_Y_L]
         LI      r0, >sprite3
-        SB      r0, [a3+OFS_BLIT_SRC_X_H]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_H]
         LI      r0, <sprite3
-        SB      r0, [a3+OFS_BLIT_SRC_X_L]
+        SB      r0, [a3+OFS2_BLIT_SRC_X_L]
         LI      r0, CMD_BLIT_MEM_TRANS
-        SB      r0, [a3+OFS_BLIT_CMD]
+        SB      r0, [a3+OFS2_BLIT_CMD]
 draw_sprites_end:
         RET
 
