124622e4d38b47a04887ab8d54601ce486f99a14
lyrah0 <lyrah0a@gmail.com>
2026-05-02 19:17:33 +0300

    initial work on character printing demo

--- software/graphics_demo.s
+++ software/graphics_demo.s
@@ -12,7 +12,7 @@ init_timer:
         SB      r0, [a3+0x21]
         LI      r0, <int_handler
         SB      r0, [a3+0x20]
-        
+
         LI      r7, >HW_BASE2
         LI      r6, <HW_BASE2
         LI      r0, 0x01
@@ -41,6 +41,7 @@ demos_start:
         BL      AL, wait
         LI      r3, 0x01                ; skip wait
         BL      AL, draw_rects
+        BL      AL, print_chars
 idle_loop:
         WFI
         B       AL, idle_loop
@@ -168,6 +169,200 @@ wait:
         B       ZC, wait
         RET
 
+print_char:
+        PUSH    a3
+        MOV     r6, spl
+        MOV     r7, sph
+
+        LI      r5, >HW_BASE2
+        LI      r4, <HW_BASE2
+
+        SB      r0, [a2+OFS2_BLIT_DST_X_L]
+        SB      r1, [a2+OFS2_BLIT_DST_X_H]
+        SB      r2, [a2+OFS2_BLIT_DST_Y_L]
+        LI      r0, 0x00
+        SB      r0, [a2+OFS2_BLIT_DST_Y_H]
+        SB      r0, [a2+OFS2_BLIT_SRC_STRIDE_H]
+        SB      r0, [a2+OFS2_BLIT_WIDTH_H]
+        SB      r0, [a2+OFS2_BLIT_HEIGHT_H]
+        SB      r0, [a2+OFS2_BLIT_COLOR]
+        LI      r0, 0x80
+        SB      r0, [a2+OFS2_BLIT_SRC_STRIDE_L]
+        LI      r0, 0x08
+        SB      r0, [a2+OFS2_BLIT_WIDTH_L]
+        SB      r0, [a2+OFS2_BLIT_HEIGHT_L]
+
+        LB      r2, [a3+5]      ; src x
+        LI      r3, 0
+        LI      r0, 8
+        LI      r1, 0
+        BL      AL, multiply
+
+        PUSH    a0
+        LB      r2, [a3+6]      ; src y
+        LI      r3, 0
+        LI      r0, 0x80
+        LI      r1, 0
+        BL      AL, multiply
+
+        POP     a1
+        ADD     r0, r0, r2
+        ADC     r1, r1, r3
+
+        LI      r2, >font_8x8
+        LI      r3, <font_8x8
+        ADD     r0, r0, r2
+        ADC     r1, r1, r3
+        
+        LI      r5, >HW_BASE2
+        LI      r4, <HW_BASE2
+        SB      r0, [a2+OFS2_BLIT_SRC_X_L]
+        SB      r1, [a2+OFS2_BLIT_SRC_X_H]
+        LI      r0, CMD_BLIT_MEM_TRANS
+        SB      r0, [a2+OFS2_BLIT_CMD]
+print_char_end:
+        MOV     spl, r6
+        MOV     sph, r7
+        POP     a3
+        RET
+
+
+
+
+multiply:
+        PUSH    a3
+        MOV     r6, spl
+        MOV     r7, sph
+
+        MOV     r0, r4
+        MOV     r1, r5
+
+        LI      r0, 0
+        LI      r1, 0
+
+multiply_loop:
+        CMA     r2, r2
+        B       ZC, multiply_continue
+        CMA     r3, r3
+        B       ZS, multiply_end
+multiply_continue:
+        ADD     r2, r2, -1
+        DECB    r3
+        ADD     r0, r0, r4
+        ADC     r1, r1, r5
+        B       AL, multiply_loop
+multiply_end:
+        MOV     spl, r6
+        MOV     sph, r7
+        POP     a3
+        RET
+
+
+print_chars:
+        ; H
+        LI      r0, 7
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x00
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; E
+        LI      r0, 4
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x08
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; L
+        LI      r0, 11
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x10
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; L
+        LI      r0, 11
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x18
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; O
+        LI      r0, 14
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x20
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; W
+        LI      r0, 6
+        LI      r1, 1
+        PUSH    a0
+        LI      r0, 0x30
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; O
+        LI      r0, 14
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x38
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; R
+        LI      r0, 1
+        LI      r1, 1
+        PUSH    a0
+        LI      r0, 0x40
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; L
+        LI      r0, 11
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x48
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        ; D
+        LI      r0, 3
+        LI      r1, 0
+        PUSH    a0
+        LI      r0, 0x50
+        LI      r1, 0x00
+        LI      r2, 0x00
+        BL      AL, print_char
+        WFI
+        POP     a0
+        RET
+
+
+
 .bankw1 0
 .origin 0x8000
 sprite0:
@@ -209,6 +404,8 @@ sprite3:
 .db 0x00, 0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00
 .db 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00
 .db 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF
+
+.include "8x8font.s"
 
 .origin 0xC000
 int_handler:
