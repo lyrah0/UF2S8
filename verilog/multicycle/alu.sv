59a7e3b74887e5d45fdd42dc8de0145a2ab2d95d
lyrah0 <lyrah0a@gmail.com>
2026-05-17 10:33:11 +0300

    remove extended carry and simplify overflow&carry logic

--- verilog/multicycle/alu.sv
+++ verilog/multicycle/alu.sv
@@ -4,10 +4,10 @@ module alu(
 	input  logic        i_c,
 	input  logic [3:0]  i_op,
 	output logic [15:0] o_result,
-	output logic [4:0]  o_flags
+	output logic [3:0]  o_flags
 );
 
-	logic [16:0]	temp_res;
+	logic [15:0]	temp_res;
 	logic [7:0]     w_a8;
 	logic [7:0]     w_b8;
 
@@ -16,37 +16,31 @@ module alu(
 		w_b8 = i_b[7:0];
 		case (i_op)
 			4'h0: temp_res = i_a + i_b;
-			4'h1: temp_res = i_a + i_b + {16'b0, i_c};
+			4'h1: temp_res = i_a + i_b + {15'b0, i_c};
 			4'h2: temp_res = i_a - i_b;
-			4'h3: temp_res = i_a - i_b - {16'b0, ~i_c};
-			4'h4: temp_res = {9'bx, w_a8 & w_b8};
-			4'h5: temp_res = {9'bx, w_a8 | w_b8};
-			4'h6: temp_res = {9'bx, ~(w_a8 | w_b8)};
-			4'h7: temp_res = {9'bx, w_a8 ^ w_b8};
-			4'h8: temp_res = {9'bx, w_a8 << i_b[2:0]};
-			4'h9: temp_res = {9'bx, w_a8 >> i_b[2:0]};
-			4'hA: temp_res = {9'bx, w_a8 >>> i_b[2:0]};
-			4'hB: temp_res = {1'bx, i_a};
-			4'hC: temp_res = {1'bx, i_b};
-			default: temp_res = 17'bx;
+			4'h3: temp_res = i_a - i_b - {15'b0, ~i_c};
+			4'h4: temp_res = {8'bx, w_a8 & w_b8};
+			4'h5: temp_res = {8'bx, w_a8 | w_b8};
+			4'h6: temp_res = {8'bx, ~(w_a8 | w_b8)};
+			4'h7: temp_res = {8'bx, w_a8 ^ w_b8};
+			4'h8: temp_res = {8'bx, w_a8 << i_b[2:0]};
+			4'h9: temp_res = {8'bx, w_a8 >> i_b[2:0]};
+			4'hA: temp_res = {8'bx, w_a8 >>> i_b[2:0]};
+			4'hB: temp_res = i_a;
+			4'hC: temp_res = i_b;
+			default: temp_res = 16'bx;
 		endcase
 	end
 
-	assign o_result = temp_res[15:0];
+	assign o_result = temp_res;
 
 	always_comb begin
-		if (i_op == 4'h0 || i_op == 4'h1) begin
-			o_flags[3] = (i_a[7] == i_b[7]) && (i_a[7] != temp_res[7]); // Overflow
-			o_flags[0] = temp_res[8]; // Carry
-			o_flags[4] = temp_res[16]; // Extended Carry
-		end else if (i_op == 4'h2 || i_op == 4'h3) begin
+		if (i_op == 4'h2 || i_op == 4'h3) begin
 			o_flags[3] = (i_a[7] != i_b[7]) && (i_a[7] != temp_res[7]); // Overflow
 			o_flags[0] = ~temp_res[8]; // inverted Carry
-			o_flags[4] = ~temp_res[16]; // inverted Extended Carry
 		end else begin
-			o_flags[3] = 1'bx; // Overflow
-			o_flags[0] = 1'bx; // Carry
-			o_flags[4] = 1'bx; // Extended Carry
+			o_flags[3] = (i_a[7] == i_b[7]) && (i_a[7] != temp_res[7]); // Overflow
+			o_flags[0] = temp_res[8]; // Carry
 		end
 
 		o_flags[1] = (temp_res[7:0] == 8'b0); // Zero
